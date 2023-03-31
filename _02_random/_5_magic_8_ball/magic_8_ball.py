4e7e58e70d13309d1557b402639eb542a5236958
Koster2411 <nk33233@pacificcoastacademy.org>
2023-03-31 16:55:55 -0700

    I finished four games

--- _02_random/_5_magic_8_ball/magic_8_ball.py
+++ _02_random/_5_magic_8_ball/magic_8_ball.py
@@ -4,13 +4,20 @@ from tkinter import messagebox, Tk, simpledialog
 if __name__ == '__main__':
     window = Tk()
     window.withdraw()
-
-    # TODO Get the user to enter a question for the 8 ball to answer
-    messagebox = simpledialog.askstring(title=(question), prompt=("type in a yes or no question"))
-    # Make a variable and initialize it to a random number between 0 and 3
     number = random.randint(0, 3)
+    # TODO Get the user to enter a question for the 8 ball to answer
+    messagebox_ = simpledialog.askstring(title=(number), prompt=("type in a yes or no question"))
+    # Make a variable and initialize it to a random number between 0 and 3
 
-    for
+
+    if number == 0:
+        messagebox.showinfo(title=("yes"), message=("yes"))
+    elif number == 1:
+        messagebox.showinfo(title=("no"), message=("no"))
+    elif number == 2:
+        messagebox.showinfo(title=("Google"), message=("Maybe you should ask google"))
+    elif number == 3:
+        messagebox.showinfo(title=("something"), message=("Maybe you know the answer"))
     # If the random number is 0
 
         # tell the user "Yes"
--- _02_random/_6_lottery_numbers/lottery_numbers.py
+++ _02_random/_6_lottery_numbers/lottery_numbers.py
@@ -7,7 +7,20 @@ if __name__ == '__main__':
     window.withdraw()
 
     # TODO 1) Get 6 random numbers to put on your lottery ticket
-
+    numbers = random.randint(1, 99)
+    Number = random.randint (1, 99)
+    Numbers = random.randint (1, 99)
+    Number4 = random.randint (1, 99)
+    Number5 = random.randint (1, 99)
+    Number6 = random.randint (1, 99)
+    print(numbers)
+    print(Number)
+    print(Numbers)
+    print(Number4)
+    print(Number5)
+    print(Number6)
+    []
     # TODO 2) Display the selected numbers to the user in a pop-up
+    messagebox.showinfo(title=("Lottery Ticket"), message=(numbers,Number,Numbers,Number4,Number5,Number6))
 
     # TODO 3) BONUS: Set the title of the pop-up to show it is a lottery ticket
